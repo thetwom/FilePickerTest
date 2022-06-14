23d177814fbf8ebf051d57fb7fa6014eac9cb531
thetwom <michael@moekadu.de>
2022-06-14 21:17:23 +0200

    Simplify test and demonstrate the open-document bug.

--- app/src/main/java/de/moekadu/filepickertest/MainActivity.kt
+++ app/src/main/java/de/moekadu/filepickertest/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContract
@@ -16,15 +17,32 @@ import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
 
+    private val readFile = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+        Log.v("FilePickerTest", "MainActivity.readFile : uri ? $uri")
+        if (uri != null) {
+            val fileContent = contentResolver?.openInputStream(uri)?.use { stream ->
+                stream.reader().use {
+                    it.readText()
+                }
+            }
+            textView?.text = fileContent
+        } else {
+            textView?.text = "Failed to read file!"
+        }
+    }
+
+    private var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace<MainFragment>(R.id.fragment_container)
-            }
+        textView = findViewById(R.id.text)
+
+        val button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener {
+            readFile.launch(arrayOf("text/plain"))
         }
+
     }
 }
